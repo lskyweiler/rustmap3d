252d116bfcdf09a5ed7a74776b6e04ec9aae7896
Lucas Weiler <lucas.weiler@dynetics.com>
2026-01-16 14:31:01 -0600

    docs: added map3d lib docstring

--- crates/map3d/src/lib.rs
+++ crates/map3d/src/lib.rs
@@ -1,10 +1,17 @@
-mod constants;
-mod traits;
-mod transforms;
-pub mod utils;
-mod vincenty;
+/*!
+map3d is a simple geodetic coordinate conversion library
+*/
 
+mod constants;
 pub use constants::*;
+
+mod traits;
 pub use traits::*;
+
+mod transforms;
 pub use transforms::*;
+
+mod vincenty;
 pub use vincenty::*;
+
+pub mod utils;
